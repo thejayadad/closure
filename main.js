703fbc0d414e5e0c60e639c93797678f352a6ac0
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-14 18:41:06 -0400

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+function whatIate(array){
+    let meal = array;
+    function meals(){
+        meal.push("smoothie")
+
+    }
+    meals();
+    return meal;
+}
+
+console.log(whatIate(["salmon"]))
